3cd80d9e331684408843c13439ff606d08bcd118
github daisukenoguchi <daisukenoguemon@gmail.com>
2021-06-05 19:17:10 +0900

    add env for restart.sh

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@ set -e
 cd /home/isucon/isubata/webapp/go
 git pull origin master
 make build
-./isubata
+ISUBATA_DB_HOST=35.221.100.66 ISUBATA_DB_PASSWORD=isucon ISUBATA_DB_USER=isucon ./isubata
 sudo nginx -s reload
