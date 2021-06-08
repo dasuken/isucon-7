783e8a7d7b9d33b4bdf77d72df24e55303b1899b
github daisukenoguchi <daisukenoguemon@gmail.com>
2021-06-08 22:01:22 +0900

    fix redis client

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -239,12 +239,18 @@ func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM message WHERE id > 10000")
 	db.MustExec("DELETE FROM haveread")
 
+	keys, err := redisClient.Keys(context.Background(), "haveread/*").Result()
+	if err != nil {
+		panic(err)
+	}
+	redisClient.Del(context.Background(), keys...)
+
 	redisClient.Del(context.Background(), "message_count")
 	counts := []struct {
 		Count     int `db:"cnt"`
 		ChannelID int `db:"channel_id"`
 	}{}
-	err := db.Select(&counts, "SELECT COUNT(*) as cnt, channel_id FROM message group by channel_id")
+	err = db.Select(&counts, "SELECT COUNT(*) as cnt, channel_id FROM message group by channel_id")
 	if err != nil {
 		return err
 	}
